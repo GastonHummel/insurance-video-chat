6337df166f857f4a065e7236c344bab7a0fa810c
Gaston Hummel <gaston.hummel@precisely.com>
2021-03-01 16:05:24 +0100

    url & chatbot

--- language/en.js
+++ language/en.js
@@ -57,7 +57,7 @@ var Translations = {
       icon: 'share'
     },
     button3: {
-      url: '#',
+      url: 'https://bot.dcsdemo.nl/home',
       label: 'Policy\nDocuments',
       icon: 'website'
     },
